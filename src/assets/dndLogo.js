0e4552d36fcae32c2f6367d43c74da4e035027ce
Henrique_Veras <theveras1@gmail.com>
2022-05-05 03:45:42 -0300

    header dnd basico feito

---
+++ src/assets/dndLogo.js
@@ -0,0 +1,9 @@
+ import React from 'react'
+ 
+ export default function DndLogo() {
+   return (
+     <div>
+         <img className='md:h-[40px] h-[45px]' src="https://images.ctfassets.net/swt2dsco9mfe/1qU2YMq2MSczf9KGme5gu7/a580e2f158f2c08faad3a9d4463af0bc/dnd-hub-logo.png?fm=avif" alt="" />
+     </div>
+   )
+ }
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {LinkIcon} from '@heroicons/react/outline';
+import { MenuIcon } from '@heroicons/react/outline';
+import DndLogo from '../assets/dndLogo';
+
+export default function Header() {
+  return (
+    <div className='w-full h-[60px] z-10 bg-[#dfddd6] px-3 fixed border-b-4 border-red-700'>
+			<div className='md:w-full md:h-full flex md:items-center justify-center md:justify-between md:px-3 xl:px-[20%]'>
+				<MenuIcon className='w-8 absolute top-3 left-10 md:hidden text-zinc-500'/>
+				<div className='flex items-center pt-2'>
+						<DndLogo />
+				</div>
+				<div className='hidden md:flex justify-between'>
+					<h2 className='px-4 text-1xl text-zinc-700 font-bold'>PRODUCTS</h2>
+					<h2 className='px-4 text-1xl text-zinc-700 font-bold'>GAMEPLAY</h2>
+					<h2 className='px-4 text-1xl text-zinc-700 font-bold'>STORY</h2>
+					<h2 className='px-4 text-1xl text-zinc-700 font-bold'>EVENTS</h2>
+				</div>
+				<div className='hidden md:flex'>
+					<h2 className='text-1xl px-4 text-zinc-700'>Store Locator</h2>
+					<h2 className='text-1xl text-zinc-700'>Account</h2>
+				</div>
+			</div>
+    </div>
+  )
+}
