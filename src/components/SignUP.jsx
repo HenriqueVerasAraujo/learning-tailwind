942a42557250b9368b2965755b859790692b40ce
Henrique_Veras <theveras1@gmail.com>
2022-05-12 23:01:38 -0300

    estrutura basica do componente criada

---
+++ src/components/SignUP.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SingleButton from './SingleButton'
+
+export default function SignUP() {
+  return (
+    <div className='bg-red-600 w-full h-[350px] relative'>
+        <div className='w-full h-full flex font-Roboto absolute z-10 px-[300px]'>
+            <img  className='pr-10' src="https://images.ctfassets.net/swt2dsco9mfe/6Kea8mPfG9Ba1RLniuJGlo/b6c63cfc2dfb1b4106e7d45f7754bb8a/D7h01oScW0cn-productth-lorecard.png?w=406&h=312&q=70&fit=thumb&fm=avif" alt="/" />
+            <div className='flex flex-col justify-center items-start pl-10'>
+                <h1 className='text-red-800 font-extrabold text-4xl pb-10'>Get Digital D&D Content</h1>
+                <h1 className='text-zinc-800 text-2xl pb-1'>Sign up now for a Wizards Account, and gain access to the digital Monstrous Compendium!</h1>
+                <h1 className='text-zinc-800 font-2xl pb-5'> (Already have a Wizards Account? Sign in now.)</h1>
+                <SingleButton text='SIGN UP NOW'/>
+
+            </div>
+        </div>
+        <img  className='object-cover w-full h-full absolute z-0' src="https://images.ctfassets.net/swt2dsco9mfe/VThip0PtJa6THbBMULIQw/f21209649170517a6053e9a128281cd7/oDh01ocW0cn-product-ctablock.jpg?q=70&w=1920&fm=webp" alt="/" />
+    </div>
+  )
+}
--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FeatureSection from '../components/FeatureSection'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import SignUp from '../components/SignUP'
 
 export default function MainPage() {
   return (
@@ -9,6 +10,7 @@ export default function MainPage() {
       <Header />
       <Hero />
       <FeatureSection />
+      <SignUp />
     </div>
   )
 }
